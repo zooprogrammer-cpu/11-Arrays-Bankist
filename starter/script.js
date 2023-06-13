bb27ac05f22055c0e3be4fc05ed7e84908869b45
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-06-12 23:45:05 -0500

    map

--- starter/script.js
+++ starter/script.js
@@ -83,6 +83,28 @@ displayMovements(account1.movements);
 console.log(containerMovements.innerHTML);
 /////////////////////////////////////////////////
 
+// Maps
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurTousd = 1.1;
+const movementsUSD = movements.map((mov)=>{
+  return mov * eurTousd; 
+})
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsDescriptions = movements.map((mov, i)=>{
+  return  `Movement ${i + 1} You ${mov > 0 ? 'deposited' :
+   'withdrew'} ${Math.abs(mov)}`
+  // if (mov > 0) {
+  //   return (`Movement ${i + 1}: You deposited ${mov}`);
+  // } else {
+  //   return (`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  // }
+})
+
+console.log(movementsDescriptions);
 
 
 
