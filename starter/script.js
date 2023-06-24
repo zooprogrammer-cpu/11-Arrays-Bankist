968ec5d0bc46da61153966a42712451174464b84
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-06-24 12:32:19 -0500

    Implementing login

--- starter/script.js
+++ starter/script.js
@@ -77,7 +77,7 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayMovements(account1.movements);
+// displayMovements(account1.movements);
 
 // Sum all balance using reduce 
 const calcDisplayBalance = (movements) => {
@@ -87,20 +87,20 @@ const calcDisplayBalance = (movements) => {
   labelBalance.textContent = `${balance} €`;
 
 };
-calcDisplayBalance(account1.movements);
+// calcDisplayBalance(account1.movements);
 
 // Sum only the income and out using filter and reduce
-const calcDisplaySummary = (movements) => {
-  const incomes = movements.filter(mov=> mov > 0)    
+const calcDisplaySummary = (acc) => {
+  const incomes = acc.movements.filter(mov=> mov > 0)    
   .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent =`${incomes}€`;
 
-  const out = movements.filter(mov=> mov < 0)
+  const out = acc.movements.filter(mov=> mov < 0)
   .reduce((acc,mov) =>acc +mov, 0);
   labelSumOut.textContent =`${Math.abs(out)}€`;
 
-  const interest = movements.filter(mov=> mov >0)
-  .map(deposit => deposit * 1.2/100)
+  const interest = acc.movements.filter(mov=> mov >0)
+  .map(deposit => deposit * acc.interestRate/100)
   .filter((int, i, arr)=> {
     console.log(arr);
     return int >= 1; // give interest on only 1
@@ -110,7 +110,7 @@ const calcDisplaySummary = (movements) => {
 
 };
 
-calcDisplaySummary(account1.movements);
+// calcDisplaySummary(account1.movements);
 
 // Adds a username property to each account without using push or add
 // used forEach to have "side-effects" without returning anything, just did some work
@@ -125,101 +125,50 @@ const createUsernames = function (accs) {
 };
 
 createUsernames(accounts);
-
-//////////////////////////////////////
-// Coding Challenge #2
-
-
-// Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
-
-// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
-
-// 1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
-// 2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
-// 3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
-// 4. Run the function for both test datasets
-
-// TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
-// TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
-// const dogAges1 = [5, 2, 4, 1, 15, 8, 3];
-// const dogAges2 = [16, 6, 10, 5, 6, 1, 4];
-// const calcAverageHumanAge = function (ages) {
-//   const humanAges = ages.map(age => {
-//     return age <= 2 ? age * 2 : (16 + age * 4);
-//   })
-//   const adults = humanAges.filter((age)=>{
-//     return age > 18
-//   })
-//   console.log(humanAges);
-//   console.log(adults);
-//   const average = adults.reduce((acc, age)=>{
-//     return (acc + age);
-//   },0) / adults.length
-//   return average; 
-// }
-
-// console.log(calcAverageHumanAge(dogAges1));
-// console.log(calcAverageHumanAge(dogAges2));
-
-// Chaining Methods
-const movements =  [200, 450, -400, 3000, -650, -130, 70, 1300];
-const eurToUsd = 1.1;
-const totalDeposits = movements
-  .filter(mov=> mov > 0)
-  // .map(mov=> mov * eurToUsd)
-  .map((mov, i, arr)=>{
-    console.log('array where map method is called on: ',arr);
-    return mov * eurToUsd;
-  })
-  .reduce((acc, mov)=> acc + mov, 0)
-console.log('totalDeposits', totalDeposits);
-
-//////////////////////////////////////
-// Coding Challenge #3
-
-/* 
-Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
-
-TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
-TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
-
-GOOD LUCK 😀
-*/
-// const dogAges1 = [5, 2, 4, 1, 15, 8, 3];
-// const dogAges2 = [16, 6, 10, 5, 6, 1, 4];
-// const calcAverageHumanAge = function (ages) {
-//   const humanAges = ages.map(age => {
-//     return age <= 2 ? age * 2 : (16 + age * 4);
-//   })
-//   const adults = humanAges.filter((age)=>{
-//     return age > 18
-//   })
-//   console.log(humanAges);
-//   console.log(adults);
-//   const average = adults.reduce((acc, age)=>{
-//     return (acc + age);
-//   },0) / adults.length
-//   return average; 
-// }
-
-const dogAges1 = [5, 2, 4, 1, 15, 8, 3];
-const dogAges2 = [16, 6, 10, 5, 6, 1, 4];
-const calcAverageHumanAge = ages => 
-  ages.map(age => (age <= 2 ? 2 * age : 16 + age *4))
-  .filter(age => age >= 18)
-  .reduce((acc, age, i, arr)=> acc + age /arr.length, 0);
-
-
-console.log(calcAverageHumanAge(dogAges1)); //44
-console.log(calcAverageHumanAge(dogAges2)); //47.33
-
-
-
-// find method - returns the first elelemt in the array that satisfies the condition
-const firstWithdrawl = movements.find(mov => mov < 0);
-console.log('firstWithdrawl: ',firstWithdrawl);
-
 console.log(accounts);
-const account = accounts.find(acc => acc.owner === 'Jessica Davis');
-console.log(account);
+
+// Username and pin log in
+
+// Event Handlers
+// When you click this, LOGIN gets printed
+// but page reloads quickly since that is the default behavior
+// Enter is the same as user clicking login
+let currentAccount; 
+btnLogin.addEventListener('click', function(e){
+  //call preventDefault method to prevent form for submitting
+  e.preventDefault(); 
+  console.log('LOGIN');
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+    );
+    console.log(currentAccount);
+    if (currentAccount?.pin === Number(inputLoginPin.value)){
+      console.log('PIN LOGIN');
+      // Display UI and welcome message
+      labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`;
+      containerApp.style.opacity = 100; // default opactiy on css is 0. 
+      
+      // Clear login input and pin fields
+      inputLoginUsername.value = inputLoginPin.value = '';
+      // Remove forcus from pin fleid
+      inputLoginPin.blur(); 
+      // Display movements
+      displayMovements(currentAccount.movements);
+      // Display balance 
+      calcDisplayBalance(currentAccount.movements);
+      // Display summary
+      calcDisplaySummary(currentAccount);
+    
+    }
+});
+
+
+
+
+
+
+
+
+
 
